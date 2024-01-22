fc6183440dfa568e54ea9f64efc01b41974eff33
Kishan Gupta <kishangupta4514@gmail.com>
2024-01-22 23:55:37 +0530

    update index.js

--- server/index.js
+++ server/index.js
@@ -22,10 +22,11 @@ database.connect();
 app.use(express.json());
 app.use(cookieParser());
 app.use(
-	cors({
-		origin:"http://localhost:3000",
-		credentials:true,
-	})
+	cors()
+	// cors({
+	// 	origin:"http://localhost:3000",
+	// 	credentials:true,
+	// })
 )
 
 app.use(
