fd37ed988e9d2c1fa17b230bad25ec51dde1dca3
Kishan Gupta <kishangupta4514@gmail.com>
2024-01-23 00:26:03 +0530

    update

--- server/index.js
+++ server/index.js
@@ -28,6 +28,18 @@ app.use(
 	// 	credentials:true,
 	// })
 )
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://studynotion-frontend-eta.vercel.app/');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 
 app.use(
 	fileUpload({
